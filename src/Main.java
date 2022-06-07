17a0c170117dcfa42ef38f8745308e9b3b0adeda
KhonSS <xonchik_95@mail.ru>
2022-06-07 17:41:39 +0900

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println ("Пишем наше дз!");
+        System.out.println ("Создаем коммит и делаем пуш");
+        System.out.println ("Преходим в ГитХаб");
     }
 }
